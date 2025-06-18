ca18267fd556d1f287d74acc6d149c88f9d1348b
dannykhan02 <dannykhan614@gmail.com>
2025-06-18 20:56:08 +0300

    Update requirement.txt

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ db = SQLAlchemy()
 class TokenBlocklist(db.Model):
     id = db.Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     jti = db.Column(db.String(36), nullable=False, index=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(String(36), nullable=False)  
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 # Enum definitions
